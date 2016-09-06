5c189db5bbd2b4e12a34bb6aec12f79a367c4044
xmlking <xmlking@gmail.com>
2016-09-06 03:34:25 -0700

    CORS

--- trust-broker/grails-app/conf/spring/resources.groovy
+++ trust-broker/grails-app/conf/spring/resources.groovy
@@ -1,7 +1,9 @@
+import trustbroker.CorsFilter
 import trustbroker.CustomUserDetailsService
 
 // Place your Spring DSL code here
 beans = {
+    corsFilter(CorsFilter)
     userDetailsService(CustomUserDetailsService)
     // TODO
     // customClaimProvider(MultiTenantCustomClaimProvider)
--- trust-broker/grails-app/init/trustbroker/Application.groovy
+++ trust-broker/grails-app/init/trustbroker/Application.groovy
@@ -13,15 +13,17 @@ class Application extends GrailsAutoConfiguration {
         GrailsApp.run(Application, args)
     }
 
+    //FIXME make this work and remove my CorsFilter
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 //registry.addMapping("/api/login").allowedOrigins("http://localhost:9000")
-                //registry.addMapping("/api/**").allowedOrigins("http://localhost:3000").maxAge(3600);
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:8080").maxAge(3600);
                 //registry.addMapping("/**/*").allowedOrigins("*");
-                registry.addMapping("/**");
+                //registry.addMapping("/**");
+
             }
         };
     }
